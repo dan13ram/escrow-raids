4145babe3941d2ba769f68360c1078e50947dcf0
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-07-23 10:14:51 -0400

    Update LexGuildLocker.sol
    remove unused balanceOf function

--- LexGuildLocker.sol
+++ LexGuildLocker.sol
@@ -89,8 +89,6 @@ library Address { // helper function for address type / openzeppelin-contracts/b
 }
 
 interface IERC20 { // brief interface for erc20 token txs
-    function balanceOf(address who) external view returns (uint256);
-    
     function transfer(address to, uint256 value) external returns (bool);
 
     function transferFrom(address from, address to, uint256 value) external returns (bool);
